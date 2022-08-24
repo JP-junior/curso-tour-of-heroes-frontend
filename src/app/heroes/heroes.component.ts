fb57ba7485879de4fee8ca64024a6227622c4752
Jorge Paduanelli <jorge.paduanelli.junior@gmail.com>
2022-08-23 21:10:12 -0300

    Separando detalhes do herói

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
-
   heroes = HEROES;
   selectedHero?: Hero;
 
