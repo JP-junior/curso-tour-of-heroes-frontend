129bccbf918e32430fa0ffa1726c149808d35259
Jorge Paduanelli <jorge.paduanelli.junior@gmail.com>
2022-09-26 20:03:30 -0300

    #25 - Loading...

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -5,14 +6,23 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-const COMPONENTS = [ToolbarComponent, MessagesComponent];
+const COMPONENTS = [ToolbarComponent, MessagesComponent,LoadingComponent];
 const MODULES = [FlexLayoutModule, MaterialModule, RouterModule];
 
 @NgModule({
   declarations: [COMPONENTS],
   imports: [CommonModule, MODULES],
   exports: [COMPONENTS, MODULES],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule?: CoreModule){
